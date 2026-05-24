562ab392da4723f5740dcb85d56df2b8c9fe4202
Meet Maratha <meet.maratha6@gmail.com>
2026-05-23 23:44:41 -0300

    feat: added education section

--- script.js
+++ script.js
@@ -9,3 +9,4 @@ loadSection("hero", "sections/hero.html");
 loadSection("projects", "sections/projects.html");
 loadSection("skills", "sections/skills.html");
 loadSection("experience", "sections/experience.html");
+loadSection("education", "sections/education.html");
